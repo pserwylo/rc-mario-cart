f7e00bb49c0d011c2368196088992330bbc94d45
Peter Serwylo <peter@ivt.com.au>
2013-08-08 07:35:42 +1000

    Two quick presses of 'Right' will cause an action to occur.

--- rc_intercept.ino
+++ rc_intercept.ino
@@ -2,7 +2,14 @@
 // RX chip is connected to.
 #define RC_INPUT_RIGHT    5
 
+// Duration between presses (in ms) to count as 
+// an "action" - in my case, "fire infrared" 
+#define ACTION_DURATION 500
+
 boolean isActive = false;
+
+// Last time the "Right" function was activated
+long unsigned int lastActivated = 0;
 
 void setup() {
   Serial.begin( 9600 );
@@ -10,25 +17,45 @@ void setup() {
 }
 
 /**
+ * An action was pressed twice in quick succession.
+ * We can use queues like this to mean, e.g. "Fire IR LED" in the future.
+ */
+void actionPerformed() {
+  
+  // For now, just log this to the serial port for debugging...
+  Serial.println( "Received action from Right pin" );
+  
+  // TODO: Hook up IR LED and "fire" it when an action is performed.
+
+}
+
+/**
  * Continuously read input from the RX chip. 
  * If the "Right" function is activated or deactivated, log it and remember it.
+ * Also, if it is identified for the second time in ACTION_DURATION
+ * milliseconds, then invoke actionPerformed().
  */
 void loop() {
  
-   boolean isHigh = ( digitalRead( RC_INPUT_RIGHT ) == HIGH );
+  boolean isHigh = ( digitalRead( RC_INPUT_RIGHT ) == HIGH );
  
-   // Previously pressed, now released...   
-   if ( isActive && !isHigh ) {
+  // Previously pressed, now released...   
+  if ( isActive && !isHigh ) {
     
-     Serial.println( "Right OFF" );
-     isActive = false;
+    Serial.println( "Right OFF" );
+    isActive = false;
     
-   // Previously released, now pressed...
-   } else if ( !isActive && isHigh ) {
+  // Previously released, now pressed...
+  } else if ( !isActive && isHigh ) {
     
-     Serial.println( "Right ON" );
-     isActive = true;
+    Serial.println( "Right ON" );
+    isActive = true;
     
-   }
+    if ( millis() - lastActivated < ACTION_DURATION ) {
+      actionPerformed();
+    }
+    
+    lastActivated = millis();
+  }
   
 }
